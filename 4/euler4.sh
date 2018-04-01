9875cd5cf6def21aa9c4e9b15ead6f0b23681784
xandrov <xandro_vermeulen@hotmail.com>
2018-04-01 02:53:20 +0200

    ex 4

---
+++ 4/euler4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+biggest=0
+for i in {999..100}
+do
+	#for j in {999..100}
+	for((j=999; j>=$i; j--))
+	do
+		result=$(( i*j ))
+		reverse=$(rev<<<$result)
+		[ $result -eq $reverse ] && [ $result -gt $biggest ] && biggest=$result
+	done
+done
+echo $biggest
